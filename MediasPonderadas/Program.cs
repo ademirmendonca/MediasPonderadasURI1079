2dacdb2a2996d01f17f769575f25ab448d60d2f4
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-17 12:30:47 -0300

    Ponto final adicionado para teste

--- MediasPonderadas/Program.cs
+++ MediasPonderadas/Program.cs
@@ -18,7 +18,7 @@ namespace MediasPonderadas
                 double nota2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
                 double nota3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
-                //Cálculo da média final
+                //Cálculo da média final.
                 double total = (nota1 * 2.0 + nota2 * 3.0 + nota3 * 5.0) / 10.0;
 
                 Console.WriteLine(total.ToString("F1", CultureInfo.InvariantCulture));
